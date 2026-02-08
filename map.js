a6c1ef3ac329c1836cff0ba4d3566f6574cc79e3
ii013579 <ii013579@gmail.com>
2026-02-09 00:20:43 +0800

    Add files via upload

--- map.js
+++ map.js
@@ -8,31 +8,27 @@
     window.App.map = map;
 
     const baseLayers = {
-        'Google 街道圖': L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', { attribution: 'Google Maps' }),
-        'Google 衛星圖': L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', { attribution: 'Google Maps' }),
-        'OpenStreetMap': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: 'OSM' })
+'Google 街道圖': L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}'),
+        'Google 衛星圖': L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'),
+        'Google 地形圖': L.tileLayer('https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}'),
+        'OpenStreetMap': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     };
     baseLayers['Google 街道圖'].addTo(map);
     L.control.layers(baseLayers, null, { position: 'topright' }).addTo(map);
 
-    // 恢復 v1.9.6 右下角定位控制項
-    const LocateMeControl = L.Control.extend({
-        onAdd: function() {
-            const div = L.DomUtil.create('div', 'leaflet-bar leaflet-control-custom');
-            div.style.backgroundColor = 'white';
-            div.style.width = '34px';
-            div.style.height = '34px';
-            div.style.display = 'flex';
-            div.style.alignItems = 'center';
-            div.style.justifyContent = 'center';
-            div.style.cursor = 'pointer';
-            div.innerHTML = '<span class="material-symbols-outlined">my_location</span>';
-            div.title = "定位我的位置";
-            div.onclick = function() { map.locate({ setView: true, maxZoom: 16 }); };
-            return div;
+// 恢復縮放按鈕
+    L.control.zoom({ position: 'bottomright' }).addTo(map);
+
+    // 恢復定位按鈕
+    const LocateCtrl = L.Control.extend({
+        onAdd: () => {
+            const btn = L.DomUtil.create('div', 'leaflet-bar leaflet-control-custom');
+            btn.innerHTML = '<span class="material-symbols-outlined" style="line-height:30px; cursor:pointer;">my_location</span>';
+            btn.onclick = () => map.locate({ setView: true, maxZoom: 16 });
+            return btn;
         }
     });
-    map.addControl(new LocateMeControl({ position: 'bottomright' }));
+    map.addControl(new LocateCtrl({ position: 'bottomright' }));
 
     window.App.markers.addTo(map);
     window.App.geoJsonLayers.addTo(map);
